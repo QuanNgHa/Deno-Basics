6cac16c488cb9e10dd43684586c8ad5b20fdabd3
CodeXploreRePo <codexplore.channel@gmail.com>
2020-06-19 08:50:52 +0800

    Lect 1 - Bonus: Deno Metrics to understand Deno Internals and Architecture

--- 0_Intro/deno.js
+++ 0_Intro/deno.js
@@ -1,7 +1,13 @@
 const food = Deno.args[0];
+const parent = Deno.args[1];
 
-if (food === "love") {
+if (food === "love" && parent === "Ryan Dahl") {
   console.log("🦕 ... Deno is born");
 } else {
   console.log("🥚 ... This egg needs some love");
 }
+//setTimeout is Deno API, not JS
+setTimeout(() => {
+  console.log("check");
+}, 1000);
+console.table(Deno.metrics());
